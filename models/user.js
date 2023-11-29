9af4102b53286ac6f78ccd52006db33453c0ccda
Syafirapz <100488786+Syafirapz@users.noreply.github.com>
2023-11-29 20:11:06 +0700

    Update user.js

--- models/user.js
+++ models/user.js
@@ -4,9 +4,14 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db')
 
 const user = sequelize.define('user',{ 
+    id_user:{
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
     email: {
-      type : DataTypes.STRING(30),
-      primaryKey: true,
+      type : DataTypes.STRING(100),
       allowNull : false,
       validate: {
         isEmail: true
@@ -17,12 +22,27 @@ const user = sequelize.define('user',{
       type: DataTypes.STRING
     },
     name: {
-      type : DataTypes.STRING(30),
+      type : DataTypes.STRING(50),
       allowNull : false
+    },
+    gender: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    created_at: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    updated_at: {
+        type: DataTypes.DATE,
+        allowNull: false
     }
-
   }, {
-    freezeTableName: true
+    freezeTableName: true,
+    tableName: 'user',
+    timestamps: true,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
   });
 
   module.exports = user;
